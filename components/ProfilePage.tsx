4b254165bddfeb997bac092ed34c305d5a22b65d
Akhil <Macfros@gmail.com>
2023-10-29 11:38:23 +0530

    final

--- components/ProfilePage.tsx
+++ components/ProfilePage.tsx
@@ -1,5 +1,4 @@
 import { ProjectInterface, UserProfile } from '@/common.types'
-import {MouseEventHandler } from "react";
 import Image from 'next/image'
 
 import Link from 'next/link'
@@ -10,16 +9,13 @@ type Props = {
     user: UserProfile;
 }
 
-const defaultClickHandler: MouseEventHandler = () => {
-  };
-
 const ProfilePage = ({ user }: Props) => (
     <section className='flexCenter flex-col max-w-10xl w-full mx-auto paddings'>
         <section className="flexBetween max-lg:flex-col gap-10 w-full">
             <div className='flex items-start flex-col w-full'>
                 <Image src={user?.avatarUrl} width={100} height={100} className="rounded-full" alt="user image" />
                 <p className="text-4xl font-bold mt-10">{user?.name}</p>
-                <p className="md:text-5xl text-3xl font-extrabold md:mt-10 mt-5 max-w-lg">I’m Freelance Designer 👋</p>
+                <p className="md:text-5xl text-3xl font-extrabold md:mt-10 mt-5 max-w-lg">I’m a Design Enthusiast 👋</p>
 
                 <div className="flex mt-8 gap-5 w-full flex-wrap">
                     <Button
@@ -27,7 +23,6 @@ const ProfilePage = ({ user }: Props) => (
                         leftIcon="/plus-round.svg"
                         bgColor="bg-light-white-400 !w-max"
                         textColor="text-black-100"
-                        handleClick={defaultClickHandler}
                     />
                     <Link href={`mailto:${user?.email}`}>
                         <Button title="Hire Me" leftIcon="/email.svg" />
@@ -39,14 +34,14 @@ const ProfilePage = ({ user }: Props) => (
                 <Image
                     src={user?.projects?.edges[0]?.node?.image}
                     alt="project image"
-                    width={739}
+                    width={400}
                     height={554}
                     className='rounded-xl object-contain'
                 />
             ) : (
                 <Image
                     src="/profile-post.png"
-                    width={739}
+                    width={400}
                     height={554}
                     alt="project image"
                     className='rounded-xl'
